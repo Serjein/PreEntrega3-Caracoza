adde98ae9c8910d05bf60a35729bc61689dfcfa3
Iskander Caracoza <serjein12@gmail.com>
2022-11-28 15:10:31 -0600

    se modifica carrito

--- JS/carrito.js
+++ JS/carrito.js
@@ -36,24 +36,3 @@ function recuperarCarrito(){
 }
 recuperarCarrito()
 
-function verCarrito() {
-    if (carrito.length > 0) {
-        console.table(carrito)
-
-        let totalCompra = carrito.reduce((total, album) => total + album.importe, 0).toFixed(2)
-             console.log("El total de la compra es: $ " + totalCompra)
-    } else {
-        console.warn("El carrito está vacío!")
-    }
-}
-
-// function verCarrito() {
-//     if (carrito.length > 0) {
-//         console.table(carrito)
-
-//         let totalCompra = carrito.reduce((total, album) => total + album.importe, 0).toFixed(2)
-//              console.log("El total de la compra es: $ " + totalCompra)
-//     } else {
-//         console.warn("El carrito está vacío!")
-//     }
-// }
