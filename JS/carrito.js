90e9e5ddbd5ef53272f9a6705aa761e9e81dbed7
Iskander Caracoza <serjein12@gmail.com>
2022-11-29 18:18:15 -0600

    se modifica carrito

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,4 +1,7 @@
-function armarTabla(alb){
+const totalDiv = document.querySelector('#total');
+const totalCompra = document.createElement('h2')
+
+function armarTabla(alb) {
     return `<tr>
                 <td>
                 <img class="tabla__img-carrito" src="${alb.imagen}" alt="Album">
@@ -9,7 +12,7 @@ function armarTabla(alb){
                 <td>
                     <form action="number">
                         <label for="Cantidad"></label>
-                        <input id="cantidad" name="cantidad" type="number" placeholder="1" min=1 max=100
+                        <input class="cantidad" name="cantidad" type="number" placeholder="1" min=1 max=100
                             required>
                     </form>
                 </td>
@@ -19,61 +22,30 @@ function armarTabla(alb){
                 <td>
                     <button id="${alb.artista}" class="material-symbols-outlined button">X</button>
                 </td>
-                </tr>`
-              
+            </tr>`
+
 }
 
-// function recuperarCarrito(){
-//     let tablaHTML = ""
-//     const tbody = document.querySelector("tbody")
-//     const carrito = JSON.parse(localStorage.getItem("miCarrito"))
-//     if (carrito.length > 0){
-//         carrito.forEach(alb => tablaHTML += armarTabla(alb));
-//         tbody.innerHTML = tablaHTML
-//         }
-//         calcularTotalCompra()
-//     }
-// recuperarCarrito()
-
-// const total = document.querySelector('#total')
-
-// const calcularTotalCompra = () => {
-//     let total = 0;
-//     carrito.forEach((producto) => {
-//       total += producto.importe * producto.cantidad;
-//     });
-//     totalCompra.innerHTML = total;
-//     return total
-    
-//   };
-const totalDiv = document.querySelector('#total');
-const totalCompra = document.createElement('h2')
-debugger
-function recuperarCarrito(){
-const calcularTotalCompra = () => {
+function recuperarCarrito() {
+    const calcularTotalCompra = () => {
         let total = 0;
         carrito.forEach((producto) => {
-          total += producto.importe;
+            total += producto.importe;
         });
         totalCompra.innerHTML = total;
         totalDiv.append(totalCompra)
-      };
+    };
 
     let tablaHTML = ""
     const tbody = document.querySelector("tbody")
     const carrito = JSON.parse(localStorage.getItem("miCarrito"))
-    if (carrito.length > 0){
+    if (carrito.length > 0) {
         carrito.forEach(alb => tablaHTML += armarTabla(alb));
         tbody.innerHTML = tablaHTML
-        }
-
-        calcularTotalCompra()
     }
-    recuperarCarrito()
-// const total = document.querySelector(".total")
-// const subTotal = carrito.reduce((acc, album) => acc + album.importe,0).toFixed(2)
-//     total.innerHTML = `${subTotal}`
+
+    calcularTotalCompra()
+}
+recuperarCarrito()
 
 
-
-// console.log(subTotal)
